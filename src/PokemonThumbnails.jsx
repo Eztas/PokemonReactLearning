478fcb6453a1e5242ab5cba68938fa7487ac29ce
Eztas <nietzsche2669sensei@gmail.com>
2025-03-01 18:21:07 +0900

    update: ポケモン一体のみの表示,propsの概念理解, アロー関数で簡素化, 一貫性

--- src/PokemonThumbnails.jsx
+++ src/PokemonThumbnails.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-// アロー関数でコンポーネントを定義
-const PokemonThumbnails = () => {
-  return <div>PokemonThumbnails</div>;
+// アロー関数でコンポーネントを定義(一貫性などの観点, とりあえずアロー関数の方が良さそうくらいで今は良い)
+// 本来はfunctionで定義する
+
+// 親コンポーネントから子コンポーネントに引数などを介してデータを渡すための仕組み=propsの作成
+// 親から引数でid, name, image, typeを受け取り、 それをポケモン1体の情報として表示する
+const PokemonThumbnails = ({ id, name, image, type }) => {
+  return (
+    <div>
+      <div className="number">
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div className="detail-wrapper">
+        <h4>{name}</h4>
+        <h3>{type}</h3>
+      </div>
+    </div>
+  );
 };
 
 // デフォルトエクスポートする
