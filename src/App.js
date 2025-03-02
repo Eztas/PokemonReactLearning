c2f051d33d313c4bec8541d163eefd9baacd8db3
Eztas <nietzsche2669sensei@gmail.com>
2025-03-02 18:36:57 +0900

    update: 複数ポケモン対応準備, 不要ファイルの削除

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import PokemonThumbnails from './PokemonThumbnails';
 import pokemons from './Pokemon';
 import { useEffect, useState } from 'react';
@@ -9,13 +8,29 @@ function App() {
   // そうしないと、順序の保証や状態が混同し、管理しにくくなるため
 
   // APIからデータを取得する
-  const url = "https://pokeapi.co/api/v2/pokemon";
   const fusigidaneUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur" // 仮データにフシギダネのデータ
+  // パラメータにlimitを設定し、20件取得する
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
+  // ポケモンの全データを格納する
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  const getAllPokemons = () => {
+    fetch(url)
+      .then(res => res.json()) 
+      .then(data => {              // data = res.json()
+        console.log(data.results); // resultsには20件のポケモンのデータが入っている
+        setAllPokemons(data.results); // 1-20件目のポケモンのデータを格納
+
+        // 次の20件(21件目から40件目)をURLにセットする
+        setUrl(data.next);
+      })
+  }
 
   // アロー関数でポケモン1体の情報を生成
+  // まだここでも形式として関数を作っただけで、この関数の出力はUI上では出力されない
   const createPokemonObject = (pokemonUrl) => {
     fetch(pokemonUrl)
-    .then(responce => responce.json())
+    .then(res => res.json())
     .then(data => {
       console.log(data);
       // ポケモンの画像の場所
@@ -27,24 +42,14 @@ function App() {
     })
   }
 
-  // ポケモンの名前の状態管理, useStateの引数は初期値
-  const [pokemonNames, setPokemonNames] = useState([]);
-
   // useEffectの第1引数では、アロー関数で、引数 => 結果(動作内容)で定義
   useEffect(() => {
-    fetch(url)
-    .then((response) => response.json()) //2つのthenで非同期的にデータの処理
-    .then((data) => {   // data = response.json()の結果
-      console.log(data) // 1つ目のthenでレスポンスを受け取り, 2つ目のthenでjson形式のデータを取得
-      const names = [
-        data.results[0].name,
-        data.results[1].name,
-        data.results[2].name,
-      ]
-      setPokemonNames(names);
-    })
+    getAllPokemons();
     createPokemonObject(fusigidaneUrl);
-  }, [])
+  }, []) 
+  // allPokemonsが再定義されたときに再レンダリングされる必要があるため、
+  // useEffectの第2引数にgetAllPokemonsを指定する方がいい(そうしないと警告が出る)が、
+  // 本質ではないので今回は無視
 
   return (
     <div className="app-container">
@@ -52,10 +57,11 @@ function App() {
       <div className='pokemon-container'>
         <div className='all-container'>
         {/* map関数で端的にポケモンを複数分表示 */}
-          {pokemons.map((pokemon, index) => (
+          {pokemons.map((pokemon, index) => ( // 今はまだallPokemonsではなく仮データのpokemonsを使用
             <PokemonThumbnails
               id={pokemon.id}
-              name={pokemonNames[index]}
+              // 初回レンダリングの際にエラーになるので、オプショナルチェーン(?)をつける
+              name={allPokemons[index]?.name}
               image={pokemon.image}
               type={pokemon.type} 
             />
