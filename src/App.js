7ec9dccfe7d4b3e1a749275a0dd58d9583ff36e5
Eztas <nietzsche2669sensei@gmail.com>
2025-03-04 21:41:36 +0900

    add: ポケモン情報追加機能, それに伴うリロード機能, ホバー時のアイコン画像

--- src/App.js
+++ src/App.js
@@ -10,14 +10,17 @@ function App() {
 
   const [url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon?limit=${LIMIT_NUMBER}`); // APIのURLを格納
   const [pokemons, setPokemons] = useState([]); // ポケモンのデータを格納する
+  const [isReloading, setIsReloading] = useState(false); // リロード中かどうかの状態を格納
 
   const getAllPokemons = () => {
+    setIsReloading(true); // リロード中の状態をtrueにする
     fetch(url)
       .then(res => res.json()) 
       .then(data => {              // data = res.json()
         createPokemonObject(data.results); // APIで取得したポケモンの情報に関するオブジェクト生成
         setUrl(data.next); // 次の20件(21件目から40件目)をURLにセットする
       })
+      .finally(() => setIsReloading(false)); // リロード中の状態をfalseにする
   }
 
   // アロー関数でポケモン1体の情報を生成
@@ -33,6 +36,7 @@ function App() {
         const newPokemonData = {
           id: data.id, // ポケモンの番号
           name: data.name, // ポケモンの名前
+          iconImage: data.sprites.other.dream_world.front_default, // ホバー時のポケモンのアイコン画像
           image: data.sprites.other["official-artwork"].front_default, // ポケモンの画像
           type: data.types[0].type.name // ポケモンのタイプ
         }
@@ -60,6 +64,7 @@ function App() {
               key={pokemon.id} // keyを設定し, 警告を回避
               id={pokemon.id}
               name={pokemon.name}
+              iconImage={pokemon.iconImage}
               image={pokemon.image}
               type={pokemon.type} 
             />
@@ -69,9 +74,13 @@ function App() {
         {/*onClick={getAllPokemons} は関数の参照を渡すだけ*/}
         {/*onClick={getAllPokemons()} は関数の返り値を渡す, 返り値を渡すために実行する*/}
         {/*実行される->ポケモン情報追加->再レンダリング->また{}内実行->返り値を返そうと実行*/}
-        <button className='load-more' onClick={getAllPokemons}>
-          もっとみる！
-        </button>
+        {isReloading ? (
+          <div className='load-more'>Now Loading…</div>
+        ): (
+          <button className='load-more' onClick={getAllPokemons}>
+            もっとみる！
+          </button>
+        )}
       </div>
     </div>
   )
