0436e28d04938d2233bc284e897e0633496d815d
Eztas <nietzsche2669sensei@gmail.com>
2025-03-07 14:57:56 +0900

    error: createPokemonObjectをコンポーネント化するにあたって、reactの仕様の制限を食らう. 状況の一時保存

--- src/JapaneseApp.jsx
+++ src/JapaneseApp.jsx
@@ -2,7 +2,7 @@ import PokemonThumbnails from './PokemonThumbnails';
 import { useEffect, useState, useContext } from 'react';
 import { Link } from "react-router-dom";
 import { PokemonContext } from './PokemonProvider';
-import { translatePokemon } from './api/translatePokemon';
+import { createPokemonObject } from './createPokemonObject';
 
 function JapaneseApp() {
 
@@ -12,6 +12,7 @@ function JapaneseApp() {
   const {pokemons, setPokemons, url, setUrl} = useContext(PokemonContext); // ポケモンのデータを格納する
   const [isReloading, setIsReloading] = useState(false); // リロード中かどうかの状態を格納
 
+  // reloadingをuseStateで管理とかにすると、日本語ページと英語ページで競合しそうなのでここはコンポーネント化しない
   const getAllPokemons = () => {
     setIsReloading(true); // リロード中の状態をtrueにする
     fetch(url)
@@ -21,35 +22,6 @@ function JapaneseApp() {
         setUrl(data.next); // 次の20件(21件目から40件目)をURLにセットする
       })
       .finally(() => setIsReloading(false)); // リロード中の状態をfalseにする
-  }
-
-  // アロー関数でポケモン1体の情報を生成
-  // まだここでも形式として関数を作っただけで、この関数の出力はUI上では出力されない
-  const createPokemonObject = (pokemons) => {
-    // アロー関数で, 配列.forEach (引数 => 結果(動作内容))で定義
-    pokemons.forEach ((pokemon) => {
-      const pokemonUrl = pokemon.url;
-      fetch(pokemonUrl) // ただ単にfetchをしているだけだと、非同期処理のため、データ取得順が一意ではない
-      .then(res => res.json())
-      .then(data => {
-        const pokemonJa = translatePokemon(data.name, data.types[0].type.name)
-        // ポケモン1体の情報に関するオブジェクト生成
-        const newPokemonData = {
-          id: data.id, // ポケモンの番号
-          nameJa: pokemonJa.name, // ポケモンの日本名
-          nameEn: data.name, // ポケモンの英語名
-          iconImage: data.sprites.other.dream_world.front_default, // ホバー時のポケモンのアイコン画像
-          image: data.sprites.other["official-artwork"].front_default, // ポケモンの画像
-          typeJa: pokemonJa.type, // ポケモンのタイプ(日本語)
-          typeEn: data.types[0].type.name // ポケモンのタイプ(英語)
-        }
-
-        // forEachで1-20件目のポケモンのデータを格納
-        // スプレッド構文で、現在のポケモンデータを展開して、新しいポケモンデータを追加する形で状態更新
-        // 非同期であり、順番が一意ではないのでsortしながら追加
-        setPokemons(currentPokemonData => [...currentPokemonData, newPokemonData].sort((a, b) => a.id - b.id));
-      })
-    })
   }
 
   // useEffectの第1引数では、アロー関数で、引数 => 結果(動作内容)で定義
