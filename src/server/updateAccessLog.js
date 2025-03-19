76e137d4fa867b94b200e4f5aeac2048efbec2e0
Eztas <nietzsche2669sensei@gmail.com>
2025-03-19 22:17:19 +0900

    add: express.jsの使用を検討

--- src/server/updateAccessLog.js
+++ src/server/updateAccessLog.js
@@ -1,6 +1,10 @@
 import fs from 'fs'
 import accessLogJson from './accessLog.json'
-import { defineEventHandler, readBody } from 'h3';
+//import { defineEventHandler, readBody } from 'h3';
+import { Request, Response } from 'express';
+
+// expressについて
+// https://qiita.com/fe_js_engineer/items/b052918f64b2df554d0f
 
 export default defineEventHandler(async (event) => {
     if(accessLogJson.accessCount < accessLogJson.limitAccessCount){
