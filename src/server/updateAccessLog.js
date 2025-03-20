f59226ae12a24569ac0d5b564bec305b06b1270d
Eztas <nietzsche2669sensei@gmail.com>
2025-03-21 00:04:50 +0900

    add: 別環境でも試してみるも失敗(記録だけコミットに残す)

--- src/server/updateAccessLog.js
+++ src/server/updateAccessLog.js
@@ -5,11 +5,27 @@ import { Request, Response } from 'express';
 
 // expressについて
 // https://qiita.com/fe_js_engineer/items/b052918f64b2df554d0f
+// https://zenn.dev/b1essk/articles/ssr-with-react-express
 
-export default defineEventHandler(async (event) => {
-    if(accessLogJson.accessCount < accessLogJson.limitAccessCount){
-        accessLogJson.accessCount = accessLogJson.accessCount + 1
-        fs.writeFileSync("src/server/accessLog.json", accessLogJson, 'utf-8');
+export const getPortfolio = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // リクエストパラメータからポートフォリオIDを取得
+      const { portfolioId } = req.params;
+  
+      // ポートフォリオIDを元に、ポートフォリオを取得するパラメータを設定
+      // ポートフォリオに紐づくプロフィールと、いいねを取得する
+      const searchMethod = {
+        where: { id: portfolioId },
+        include: {
+          profile: true,
+          like: true,
+        },
+      };
+  
+      const portfolio: Portfolio = await prisma.portfolios.findUniqueOrThrow(searchMethod);
+  
+      res.send({ portfolio });
+    } catch (error) {
+      next(error);
     }
-    return { message: 'Success' };
-});
+  };
